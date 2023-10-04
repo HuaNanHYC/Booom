f1c2364d253f6fc7724523db90646da569d9924c
Chyhe <2101922399@qq.com>
2023-10-04 20:43:27 +0800

    关卡配置

--- Assets/Script/test/test.cs
+++ Assets/Script/test/test.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class test : MonoBehaviour
 {
     public void getbullet()
@@ -11,5 +11,9 @@ public class test : MonoBehaviour
         InventoryManager.Instance.AddBullet(10003);
         InventoryManager.Instance.AddBullet(10004);
         InventoryManager.Instance.SavePlayerData();
+    }
+    public void NextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
